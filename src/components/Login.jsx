9e3ccfed9d72ee3fe90f1ec1ac6343cab2013b19
cirellapietro <133475613+cirellapietro@users.noreply.github.com>
2025-09-30 13:03:41 +0200

    Create Login.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+//src/components/Login.jsx
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Aggiungi form login qui */}
+    </div>
+  )
+}
+
+export default Login
