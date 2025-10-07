b9b30c4bcbe068b119729def02f9ccd41d5653fd
cirellapietro <133475613+cirellapietro@users.noreply.github.com>
2025-10-07 12:28:41 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,11 +1,7 @@
 //sec/lib/supabase.js
-qimport { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Mancano le variabili d\'ambiente Supabase')
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
