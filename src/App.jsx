f7cd75050deeb3e9756a0958c93ca3ee3e34442d
cirellapietro <133475613+cirellapietro@users.noreply.github.com>
2026-01-16 22:12:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { TrackingProvider } from "./context/TrackingContext";
 import Dashboard from "./pages/Dashboard";
-import VehiclesPage from "./pages/VehiclesPage";
+
+import VeicoliPage from "./pages/VeicoliPage";
 
 export default function App() {
   const profiloutente_id = "RECUPERATO_DA_SUPABASE";
